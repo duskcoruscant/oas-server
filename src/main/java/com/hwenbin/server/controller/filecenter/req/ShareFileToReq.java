976b7bae90f48ddee049fb41ff7af07a3969d6b6
hwb <hwenbinjobs@gmail.com>
2022-05-23 11:26:12 +0800

    valid

--- src/main/java/com/hwenbin/server/controller/filecenter/req/ShareFileToReq.java
+++ src/main/java/com/hwenbin/server/controller/filecenter/req/ShareFileToReq.java
@@ -2,6 +2,8 @@ package com.hwenbin.server.controller.filecenter.req;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author hwb
  * @date 2022/05/22 22:33
@@ -12,11 +14,13 @@ public class ShareFileToReq {
     /**
      * 文件ID
      */
+    @NotNull(message = "文件ID不能为空")
     private Long fileId;
 
     /**
      * 文件挂载目录
      */
+    @NotNull(message = "文件挂载目录不能为空")
     private Long mountFolderId;
 
 }
